e4acd42e393de6f3c34ac35181fb7d205937282e
Naman Gogia <namangogia2001@gmail.com>
2021-09-05 23:06:22 +0530

    Adding multiple cell selection

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
     for (let j = 1; j <= 100; j++) {
       let ColCode = $(`.colId-${j}`).attr("id").split("-")[2];
       CellRow.append(
-        `<div class="input-cell" id="row-${i}-col-${j}-colcode-${ColCode}" contenteditable="true"></div>`
+        `<div class="input-cell colcode-${ColCode}" id="row-${i}-col-${j}" contenteditable="false"></div>`
       );
     }
     $(".input-cell-container").append(CellRow);
@@ -50,9 +50,81 @@ $(document).ready(function () {
     $(this).toggleClass("selected");
   });
 
+  // Selecting cells by single click (Ctrl key must be pressed for multiple cell selection)
+
   $(".input-cell").click(function (e) {
-    e.preventDefault();
-    $(".input-cell.selected").removeClass("selected");
+    if (e.ctrlKey) {
+      let [row, col] = GetRowCol(this);
+      if (row > 1) {
+        let UpperCellSelected = $(`#row-${row - 1}-col-${col}`).hasClass(
+          "selected"
+        );
+        if (UpperCellSelected) {
+          $(this).addClass("top-cell-selected");
+          $(`#row-${row - 1}-col-${col}`).addClass("bottom-cell-selected");
+        }
+      }
+
+      if (col > 1) {
+        let LeftCellSelected = $(`#row-${row}-col-${col - 1}`).hasClass(
+          "selected"
+        );
+        if (LeftCellSelected) {
+          $(this).addClass("left-cell-selected");
+          $(`#row-${row}-col-${col - 1}`).addClass("right-cell-selected");
+        }
+      }
+
+      if (col < 100) {
+        let RightCellSelected = $(`#row-${row}-col-${col + 1}`).hasClass(
+          "selected"
+        );
+
+        if (RightCellSelected) {
+          $(this).addClass("right-cell-selected");
+          $(`#row-${row}-col-${col + 1}`).addClass("left-cell-selected");
+        }
+      }
+
+      if (row < 100) {
+        let BottomCellSelected = $(`#row-${row + 1}-col-${col}`).hasClass(
+          "selected"
+        );
+        if (BottomCellSelected) {
+          $(this).addClass("bottom-cell-selected");
+          $(`#row-${row + 1}-col-${col}`).addClass("top-cell-selected");
+        }
+      }
+    } else {
+      $(".input-cell.selected").removeClass("selected");
+      $(".input-cell.left-cell-selected").removeClass("left-cell-selected");
+      $(".input-cell.right-cell-selected").removeClass("right-cell-selected");
+      $(".input-cell.top-cell-selected").removeClass("top-cell-selected");
+      $(".input-cell.bottom-cell-selected").removeClass("bottom-cell-selected");
+    }
     $(this).addClass("selected");
   });
+
+  // Making cells editable by double-click
+
+  $(".input-cell").dblclick(function () {
+    $(".input-cell.selected").removeClass("selected");
+    $(this).addClass("selected");
+    $(this).attr("contenteditable", "true");
+    $(this).focus();
+  });
+
+  // Making row and column name containers scroll with the input cell container
+
+  $(".input-cell-container").scroll(function () {
+    $(".col-name-container").scrollLeft(this.scrollLeft);
+    $(".row-name-container").scrollTop(this.scrollTop);
+  });
 });
+
+function GetRowCol(e) {
+  let idArray = $(e).attr("id").split("-");
+  let row = parseInt(idArray[1]);
+  let col = parseInt(idArray[3]);
+  return [row, col];
+}
